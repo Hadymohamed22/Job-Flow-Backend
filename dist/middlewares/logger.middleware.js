ec932dbc2581b61c3acc9430dfd576d273f8d9f5
Hadymohamed22 <Hadymohamed20051022@gmail.com>
2026-09-04 15:48:40 +0300

    move dist folder to src and user server port

--- dist/middlewares/logger.middleware.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const logger = (req, res, next) => {
-    console.log(`${req.url} ${req.method}`);
-    next();
-};
-exports.default = logger;
-//# sourceMappingURL=logger.middleware.js.map
--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ import path from "node:path";
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
